290ea82cbe932296ce00ab4708c5f3e6203d2ad7
eonist <30n1st@gmail.com>
2020-07-18 17:04:15 +0200

    Update Atomic.swift

--- Sources/ParallelLoop/Atomic.swift
+++ Sources/ParallelLoop/Atomic.swift
@@ -3,6 +3,7 @@ import Foundation
  * Makes values work in parallel
  * - Note: NSLock could potentially also be used
  * - Note: Similar solution but with semaphore: https://stackoverflow.com/a/42156140/5389500 and https://gist.github.com/nestserau/ce8f5e5d3f68781732374f7b1c352a5a
+ * - Note: More in-depth info around different locks: https://www.vadimbulavin.com/atomic-properties/
  * ## Examples:
  * let x: Atomic<Int> = .init(0)
  * DispatchQueue.concurrentPerform(iterations: 1000) { y in
